a370cb16e7e714229d5961a36c4b1a11a274c9e6
song940 <admin@lsong.org>
2013-11-08 10:41:49 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyWorld::Application.config.secret_key_base = 'e216442ff54642c9a8369359c3de08abd43530b9cfcef18fbe451ac0f78050d53b94e4c91ac5a2fec8842585517b6b3e77490bb31da45bf6d50ec1332e1263aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyWorld::Application.config.session_store :cookie_store, key: '_ruby-world_session'
